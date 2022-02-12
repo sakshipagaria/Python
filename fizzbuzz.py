d217879f70c990e5b9a94febba696a51d1f216d8
Sakshi Pagariya <96334868+sakshipagaria@users.noreply.github.com>
2022-02-12 11:39:34 +0530

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,11 @@
 for i in range(1,51):
-    if(i%3==0):
-        print(str(i)+" = Fizz")
+    if(i%3==0 and i%5==0):
+        print(str(i)+" = FizzBuzz")
     else:
-       if(i%5==0):
-           print(str(i)+" = Buzz")
+       if(i%3==0):
+           print(str(i)+" = Fizz")
        else:
-           if(i%3==0 and i%5==0):
-               print(str(i)+" = FizzBuzz")
+           if(i%5==0):
+               print(str(i)+" = Buzz")
            else:
               print(str(i))
