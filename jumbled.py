333b05587338fadee72a840205398a2ebf366a9d
Sakshi Pagariya <96334868+sakshipagaria@users.noreply.github.com>
2022-02-15 17:19:08 +0530

    Add files via upload

---
+++ jumbled.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 13 21:59:29 2022
+
+@author: ADMIN
+"""
+
+import random
+def choose():
+    words=['rainbow','cookie','sandwich','science','macbook','international']
+    pick=random.choice(words)
+    return pick
+
+def jumble(word):
+    jumbled="".join(random.sample(word,len(word)))
+    return jumbled
+
+def thank(p1n,p2n,p1,p2):
+    print(p1n,'your score is:' ,p1)
+    print(p2n,'your score is:' ,p2)
+    print("Thanks for playing")
+    
+def play():
+    p1name=input('Player1 ,Please your name')
+    p2name=input('Player2 ,Please your name')
+    pp1=0
+    pp2=0
+    turn=0
+    while(1):
+        #computertask
+        picked_word=choose()
+        #create que
+        qn=jumble(picked_word)
+        print (qn)
+        #player1
+        if turn%2==0:
+            print(p1name,'Your turn. ')
+            ans=input('Whats on my mind?')
+            if ans ==picked_word:
+                pp1=pp1+1
+                print("Your score is :" ,pp1)
+            else:
+                print('Better luck next time:' ,picked_word)
+            c=input('Press 1 to continue and 0 to quit:')
+            if c==0:
+                thank(p1name,p2name,pp1,pp2)
+                break 
+     #player2
+        else:
+          print(p2name,'Your turn. ')
+          ans=input('Whats on my mind?')
+          if ans ==picked_word:
+              pp1=pp1+1
+              print("Your score is :" ,pp2)
+          else:
+              print('Better luck next time:' ,picked_word)
+          c=input('Press 1 to continue and 0 to quit:')
+          if c==0:
+              thank(p1name,p2name,pp1,pp2)
+              break 
+        turn=turn+1
+      
+play()
