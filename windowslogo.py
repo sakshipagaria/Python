7fbf156555e23a97d3740cfa17c908fe30f01af0
Sakshi Pagariya <96334868+sakshipagaria@users.noreply.github.com>
2022-03-25 20:54:08 +0530

    Add files via upload

---
+++ windowslogo.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 25 20:36:36 2022
+
+@author: ADMIN
+"""
+
+from turtle import *
+speed(2)
+bgcolor("black")
+penup()
+goto(-50,60)
+pendown()
+color("#00adef")
+begin_fill()
+goto(100,100)
+goto(100,-100)
+goto(-50,-60)
+goto(-50,60)
+end_fill()
+color("black")
+goto(15,100)
+color("black")
+width(10)
+goto(15,-100)
+penup()
+goto(100,0)
+pendown()
+goto(-100,0)
+done()
