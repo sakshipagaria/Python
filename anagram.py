a3103780dce406f3e90512cc1c71c1a469129a86
Sakshi Pagariya <96334868+sakshipagaria@users.noreply.github.com>
2022-04-02 15:16:31 +0530

    Add files via upload

---
+++ anagram.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  2 15:06:51 2022
+
+@author: ADMIN
+"""
+
+st1=input("Enter the first string: ")
+st2=input("Enter the second string: ")
+print(sorted(st1))
+print(sorted(st1))
+
+if(sorted(st1)==sorted(st2)):
+    print("Anagrams")
+else:
+    print("Not anagrams")
+    
