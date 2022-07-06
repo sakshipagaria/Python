089c17f563c3f7c0dec1d0eb679b1bc2e8ba8de1
Sakshi Pagariya <96334868+sakshipagaria@users.noreply.github.com>
2022-07-07 00:32:17 +0530

    Add files via upload

---
+++ crush.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  7 00:05:29 2022
+
+@author: pagar
+"""
+
+from turtle import *
+color('red')
+begin_fill()
+pensize(3)
+left(50)
+forward(133)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(133)
+end_fill()
