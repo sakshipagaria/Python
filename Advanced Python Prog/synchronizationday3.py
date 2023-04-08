af03fb9030a54aafbffe82292bdccbe0755b574e
Sakshi Pagariya <96334868+sakshipagaria@users.noreply.github.com>
2023-04-07 23:32:37 -0700

    Add files via upload

---
+++ Advanced Python Prog/synchronizationday3.py
@@ -0,0 +1,53 @@
+# #Without Synchronization
+
+from threading import*
+import time
+def conferenceCall(myname):
+    print("Halo,Ich bin ")
+    time.sleep(1)
+    print(myname)
+obj1 =Thread(target=conferenceCall,args=("Sakshi",))
+obj2 =Thread(target=conferenceCall,args=("Chai",))
+#here 2 objs are going to access same method at sam etime
+obj1.start()
+obj2.start()
+
+#----------------------------------
+#with synchronization 
+#1)LOCK
+
+from threading import *
+import time
+l=Lock() #lock obj created
+def come_in(name):
+    l.acquire()
+    for i in range(2):
+        print(name)
+        print("Ja herr ",end="")
+        time.sleep(1)
+
+    l.release()
+obj1=Thread(target=come_in,args=("Sakshi",))
+obj2=Thread(target=come_in,args=("Chai",))
+obj1.start()
+obj2.start()
+
+#------------------------------------
+#problem in thread block
+
+from threading import *
+obj=Lock()
+print("Lock acquired for the first time")
+obj.acquire()
+print("lock acquired second time")
+obj.acquire()
+
+#----------------------------------
+#Rlock
+
+from threading import *
+obj=RLock()
+print("Lock acquired for the first time")
+obj.acquire()
+print("lock acquired second time")
+obj.acquire()
