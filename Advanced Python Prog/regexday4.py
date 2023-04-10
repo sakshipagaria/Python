c02481fc03bdb598a6b7aafe9990d158c65fba09
Sakshi Pagariya <96334868+sakshipagaria@users.noreply.github.com>
2023-04-11 00:51:44 +0530

    Update regexday4.py

--- Advanced Python Prog/regexday4.py
+++ Advanced Python Prog/regexday4.py
@@ -1,3 +1,24 @@
+#regular expression using compile function
+import re
+count=0 
+pattern=re.compile("wood")
+print(pattern)
+matcher=pattern.finditer("how much wood could a woodchuck chuck if a wood chuck could chuck wood")
+print(matcher)
+for i in matcher:
+    count+=1
+    print(i.start(),"",i.end(),"",i.group())
+print("The number of occurences:",count)
+
+#---------------------------------------------
+#without complie function
+import re
+count=0
+matcher=re.finditer("wood", "how much wood could a woodchuck chuck if a wood chuck could chuck wood")
+for i in matcher: #loop executes four times
+    count+=1
+    print(i.start(),"",i.end(),"",i.group())
+print("The number of occurences:",count)
 
 #-------------------------------------
 #input value taken from user at runtime 
@@ -21,3 +42,17 @@ if mtch!=None:
     print(mtch.start()," ",mtch.end())
 else:
     print("There is no match at begining level")
+
+#-----------------------------------------------------
+#example
+import re
+count=0
+with open('filehandling.txt','r') as f:
+    data=f.readline()
+pattern=re.compile("chuck")
+matcher=pattern.finditer(data)
+for i in matcher:
+    count+=1
+print("The number of occurences:",count)
+
+#---------------------------------------------
