ce6b02771f80120beda4ce8f1d81c294085bc39f
Sakshi Pagariya <96334868+sakshipagaria@users.noreply.github.com>
2023-04-24 19:13:37 +0530

    Add files via upload

---
+++ lcddisplay.py
@@ -0,0 +1,21 @@
+s = int(input())
+if s == 0:
+    print(" _ \n| |\n|_|")
+elif s == 1:
+    print("   \n  |\n  |")
+elif s == 2:
+    print(" _ \n _|\n|_ ")
+elif s == 3:
+    print(" _ \n _|\n _|")
+elif s == 4:
+    print("   \n|_|\n  |")
+elif s == 5:
+    print(" _ \n|_ \n _|")
+elif s == 6:
+    print(" _ \n|_ \n|_|")
+elif s == 7:
+    print(" _ \n  |\n  |")
+elif s == 8:
+    print(" _ \n|_|\n|_|")
+elif s == 9:
+    print(" _ \n|_|\n _|")
